0b9f6119f93d28bf91d5ad1459400b98ea70062f
maher <gomemahero.com>
2022-10-07 16:18:31 +0300

    Register with Google Credentials and itis WOOOOOOORk and send itToken and exppire data

--- config/api.js
+++ config/api.js
@@ -1,1 +1,28 @@
-export const API_URL = "https://itunes.apple.com/";
+//export const API_URL = "https://itunes.apple.com/";
+
+
+export const signup = async() =>{
+
+    
+    
+         const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAgjuw8lj2jac8iG5KtlBpkfVFlhB9ZkNk',{
+                method:"POST",
+                headers:{
+                    'Content-Type':"application/json"
+                },
+                body:JSON.stringify({
+                    email:'maker@gmail.com',
+                    password:'121244355656',
+                    returnSecureToken:true
+                })
+            })
+
+            if(!response.ok){
+                throw new Error("Something went wrong! 🛢️ 🛢️ ❌🛢️ 🛢️ ❌🛢️ 🛢️ ❌🛢️ 🛢️ ❌")
+            }
+
+            const resData = await response.json()
+            console.log(resData , '✔  🛒   ✔  🛒   ✔  🛒   ')
+
+      
+    }
--- screens/maps.jsx
+++ screens/maps.jsx
@@ -1,49 +1,17 @@
-import { useCallback, useLayoutEffect, useState } from "react";
-import { Alert, StyleSheet, Text } from "react-native";
-import MapView, { Marker } from "react-native-maps";
-import { Colors } from "react-native/Libraries/NewAppScreen";
-//import IconButton from "../components/IconButton";
+import { View, Text } from 'react-native'
+import React from 'react'
+import {signup} from '../config/api'
+import { Button } from 'react-native-paper'
 
-function Map({ navigation }) {
-  const [selectedLocation, setSelectedLocation] = useState();
-
-  const region = {
-    latitude: 59.3251706,
-    longitude: 18.0705341,
-    latitudeDelta: 0.0952,
-    longitudeDelta: 0.0521,
-  };
-
-  function selectLocationHandler(event) {
-    const lat = event.nativeEvent.coordinate.latitude;
-    const lng = event.nativeEvent.coordinate.longitude;
-
-    setSelectedLocation({ lat: lat, lng: lng });
-    console.log('Selected ---->' , selectedLocation )
-  }
-
-
+export default function Deneme() {
   return (
-    <MapView
-      style={styles.map}
-      initialRegion={region}
-      onPress={selectLocationHandler}
-    >
-      {selectedLocation && (
-        <Marker
-          title="Picked Location"
-          coordinate={{
-            latitude: selectedLocation.lat,
-            longitude: selectedLocation.lng,
-          }}
-        ></Marker>
-      )}
-    </MapView>
-  );
+    <View>
+      <Text>SignUp with me here</Text>
+
+      <Button className="mt-24" icon="camera" mode="contained" onPress={signup}>
+    Register Now
+  </Button>
+
+    </View>
+  )
 }
-
-export default Map;
-
-const styles = StyleSheet.create({
-  map: { flex: 1 },
-});
