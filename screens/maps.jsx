0fe3adc5f3c7cd5ba8d6c85e3d8d6c19cbd55aaa
maher <gomemahero.com>
2022-10-08 22:41:28 +0300

    aaa

--- screens/maps.jsx
+++ screens/maps.jsx
@@ -100,6 +100,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#40B880',
         alignSelf: 'flex-end',
       },
+      
       center: {
         alignSelf: 'center',
         top: 13,
